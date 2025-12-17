621b8abb68d7b38b3c947ef2ccf675c7c7a22fab
denadar <denadar@deloitte.com>
2025-12-17 19:40:09 +0530

    Add ktor configuration for get and post request

---
+++ composeApp/src/commonMain/kotlin/org/dev/assistant/data/ChatSessionRepository.kt
@@ -0,0 +1,4 @@
+package org.dev.assistant.data
+
+class ChatSessionRepository {
+}
---
+++ composeApp/src/commonMain/kotlin/org/dev/assistant/network/NetworkClient.kt
@@ -0,0 +1,84 @@
+package org.dev.assistant.network
+
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
+import io.ktor.http.URLProtocol
+import io.ktor.http.appendPathSegments
+import io.ktor.http.contentType
+import io.ktor.http.path
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import kotlin.collections.get
+import kotlin.invoke
+import kotlin.text.append
+
+class NetworkClient(val baseUrl: String) {
+    val client = HttpClient(CIO) {
+        install(ContentNegotiation) {
+            json(Json {
+                prettyPrint = true
+                isLenient = true
+            })
+        }
+    }
+
+    suspend inline fun <reified T> get(
+        path: String? = null,
+        pathSegment: Map<String, String> = emptyMap(),
+        queryParams: Map<String, String> = emptyMap()
+    ): Result<T> {
+        return runCatching {
+            client.get {
+                url {
+                    protocol = URLProtocol.HTTPS
+                    host = baseUrl
+                    path?.let {
+                        path(it)
+                    }
+                    pathSegment.forEach { (key, value) ->
+                        appendPathSegments(key, value)
+                    }
+                    queryParams.forEach { (key, value) ->
+                        parameters.append(key, value)
+                    }
+                }
+            }.body<T>()
+        }
+    }
+
+    suspend inline fun <reified T> post(
+        path: String? = null,
+        pathSegment: Map<String, String> = emptyMap(),
+        queryParams: Map<String, String> = emptyMap(),
+        body: Any? = null
+    ): Result<T> {
+        return runCatching {
+            client.post {
+                url {
+                    protocol = URLProtocol.HTTPS
+                    host = baseUrl
+                    path?.let {
+                        path(it)
+                    }
+                    pathSegment.forEach { (key, value) ->
+                        appendPathSegments(key, value)
+                    }
+                    queryParams.forEach { (key, value) ->
+                        parameters.append(key, value)
+                    }
+                }
+                body?.let {
+                    contentType(ContentType.Application.Json)
+                    setBody(it)
+                }
+            }.body<T>()
+        }
+    }
+
+}
